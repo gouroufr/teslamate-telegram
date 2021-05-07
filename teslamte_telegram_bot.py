4cd502021f2728cd51197c9c2fc0ad480cf8914e
gouroufr <82436149+gouroufr@users.noreply.github.com>
2021-05-07 12:44:23 +0200

    Update teslamte_telegram_bot.py

--- teslamte_telegram_bot.py
+++ teslamte_telegram_bot.py
@@ -5,7 +5,7 @@
 # Add translation to texts : Open call for other languages !
 
 # BETA version / copyleft Laurent alias gouroufr
-version = "Version 20210507-02"
+version = "Version 20210507-03"
 
 import os
 import time
